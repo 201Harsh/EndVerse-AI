28082db18f005a19234fd387c88bf3d9958b923f
Harsh <gamerpandeyharsh@gmail.com>
2025-07-16 13:52:00 +0530

    routes

--- Frontend/src/Routes/Router.jsx
+++ Frontend/src/Routes/Router.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Welcome from "../Pages/Welcome";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "../Pages/Register";
+import Login from "../Pages/Login";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Welcome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
